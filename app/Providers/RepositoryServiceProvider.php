8f1b9117c161591547b20b2db0573ea15a13c9f2
trandandangiu <trithmgcs210852@fpt.edu.vn>
2026-03-20 14:37:49 +0700

    feat:[feature/statisticsReport]

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -16,6 +16,8 @@ use App\Repositories\React\ReactRepository;
 use App\Repositories\React\ReactRepositoryInterface;
 use App\Repositories\Role\RoleRepository;
 use App\Repositories\Role\RoleRepositoryInterface;
+use App\Repositories\Statistics\StatisticsRepository;
+use App\Repositories\Statistics\StatisticsRepositoryInterface;
 use App\Repositories\Submission\SubmissionRepository;
 use App\Repositories\Submission\SubmissionRepositoryInterface;
 use App\Repositories\User\UserRepository;
@@ -43,6 +45,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->singleton(CommentRepositoryInterface::class, CommentRepository::class);
         $this->app->singleton(ViewRepositoryInterface::class, ViewRepository::class);
         $this->app->singleton(NotificationRepositoryInterface::class, NotificationRepository::class);
+        $this->app->singleton(StatisticsRepositoryInterface::class,StatisticsRepository::class);
     }
 
     /**
